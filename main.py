63649fbbf406bf47c368f2274fcd5452626c7762
aaronsackey817-sudo <aaronsackey817@gmail.com>
2026-02-28 18:54:02 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+student_name="Ryan"
+grade=7
+age=12
+
+print(student_name)
+print(grade)
+print(age)
+
+#alternative
+print(f"my name is {student_name}, i am in grade {grade} and i am{age} years old")
